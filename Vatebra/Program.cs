833b04361b4298d867c17a463a876a1f904c9ad8
Ody Mbegbu <odytrice@gmail.com>
2017-03-04 15:08:54 +0100

    End of Module 2 1

---
+++ Vatebra/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vatebra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            * What is your First Name
+            * How old are you
+            * What is your Surname
+            */
+            /*string question1 = "What is your First Name";
+
+            Console.WriteLine(question1);
+            string answer1 = Console.ReadLine();
+
+            string question2 = "How old are you";
+            Console.WriteLine(question2);
+            string answer2 = Console.ReadLine();
+ 
+
+            string question3 = "What is your Surname";
+            Console.WriteLine(question3);
+            string answer3 = Console.ReadLine();
+
+            Console.WriteLine("\n");
+            Console.WriteLine("--------------------------------------------------------------");
+            Console.WriteLine("                              Result                          ");
+            Console.WriteLine("--------------------------------------------------------------");
+            Console.WriteLine("Your first Name is: " + answer1);
+            Console.WriteLine("You are " + answer2 + " years old");
+            Console.WriteLine("Your surname is " + answer3);*/
+
+
+        }
+    }
+}
